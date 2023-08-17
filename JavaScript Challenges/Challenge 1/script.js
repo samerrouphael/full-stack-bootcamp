f490de5c6116c058fc03af12edb21ba789a7765e
samer <rouphaels@gmail.com>
2023-08-17 11:43:01 +0300

    Challenge 1 - Done with Step 3

--- JavaScript Challenges/Challenge 1/script.js
+++ JavaScript Challenges/Challenge 1/script.js
@@ -1,3 +1,13 @@
 console.log('I am linked now!');
-let x = "Samer";
+var x = "Samer";
 window.alert(x)
+
+const age = "36";
+console.log(age)
+
+let lastname = " rouphael"
+console.log(lastname)
+
+var DOB = "7/10/1986";
+console.log(DOB)
+
