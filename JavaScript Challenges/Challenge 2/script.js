ca569e24e02cf22ee51c86ae8e85947c1836a3a7
samer <rouphaels@gmail.com>
2023-08-17 13:01:23 +0300

    Challenge 2 - Done with Step1

---
+++ JavaScript Challenges/Challenge 2/script.js
@@ -0,0 +1,13 @@
+let age = 36 ;
+if (age > 20 ){
+console.log( 'you can enter the room') }
+else{ console.log('you cant enter the room')}
+
+const expression1 = 5 > 3;
+const expression2 = 10 === '10';
+const expression3 = true && false;
+const expression4 = !expression3;
+console.log(expression1)
+console.log(expression2)
+console.log(expression3)
+console.log(expression4)
