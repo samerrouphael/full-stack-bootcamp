a7c21e8041321209d66b8a2f9d79997458c51c1c
samer <rouphaels@gmail.com>
2023-08-17 12:18:13 +0300

    Challenge 1 - Done with Step 5

--- JavaScript Challenges/Challenge 1/script.js
+++ JavaScript Challenges/Challenge 1/script.js
@@ -1,13 +1,55 @@
 console.log('I am linked now!');
-var x = "Samer";
-window.alert(x)
+var Name = "Samer";
+window.alert(Name)
 
 const age = "36";
 console.log(age)
 
-let lastname = " rouphael"
+let lastname = "rouphael"
 console.log(lastname)
 
 var DOB = "7/10/1986";
 console.log(DOB)
 
+
+let x = "this is the text"
+console.log(x)
+let y = " true";
+console.log(y)
+var z = "false"
+console.log(z)
+const a = "1215"
+console.log(a)
+const b = "999"
+console.log(b)
+let c ="{}";
+console.log(c)
+let d ="[]";
+console.log(d)
+var e = "null";
+console.log(e)
+var f ="undefined"
+console.log(f)
+
+
+var mytext = " mytext";
+var mytext2 ='my text2';
+
+
+// single Easier if you wish to write html within javascript	
+//double Eliminates the need to escape apostrophes when writing   sentences in english
+
+const counter = 3;
+  const backtick = `This is my text using backticks ${counter} times`;
+  const singleQuote =
+    'This is my text using single quotations ${counter} times';
+    console.log(singleQuote);
+  const doubleQuote =
+    'This is my text using double quotations ${counter} times';
+    console.log(doubleQuote);
+  const tryThisAlso = `This is my new version, it’s the ${2 + 3}th version`;
+  console.log(tryThisAlso);
+
+  // last one give the value of 2+3
+
+  
