56210fc007f6d00ebd879e5380e85bed21f9e0a4
samer <rouphaels@gmail.com>
2023-08-22 16:31:01 +0300

    challenge4 done withstep3

--- JavaScript Challenges/Challenge 4/script.js
+++ JavaScript Challenges/Challenge 4/script.js
@@ -1,7 +1,7 @@
 let fivenumber =[1,2,3,4,5] 
 console.log(fivenumber)
 
-const generalinfo ={ Name :'Samer', age :'36',height:'180',location:'newrawda',nationality :'lebanese'}
+const generalinfo ={ Name :'Samer', age :'36',height:'180',adress:'newrawda',nationality :'lebanese'}
 console.log(generalinfo)
 
 let threenumber =[1,2,3]
@@ -14,6 +14,15 @@ const additionalinfo ={hobby:'foot',education:'telecom Eng'}
 const allinfo={...generalinfo,...additionalinfo};
 console.log(allinfo)
 
+let [first,second,third] = fivenumber;
+console.log(first);
+console.log(second)
+console.log(third)
 
+let {Name,adress,nationality} = generalinfo;
+console.log(Name);
+console.log(adress)
+
+console.log(nationality);
 
 
