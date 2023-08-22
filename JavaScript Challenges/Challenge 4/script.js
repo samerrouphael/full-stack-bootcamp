e7b8def91bab2bab95acb9e8114bce8eca23b403
samer <rouphaels@gmail.com>
2023-08-22 16:02:04 +0300

    challenge4 done withstep2

--- JavaScript Challenges/Challenge 4/script.js
+++ JavaScript Challenges/Challenge 4/script.js
@@ -4,3 +4,16 @@ console.log(fivenumber)
 const generalinfo ={ Name :'Samer', age :'36',height:'180',location:'newrawda',nationality :'lebanese'}
 console.log(generalinfo)
 
+let threenumber =[1,2,3]
+console.log(threenumber)
+
+const allnumber =[...threenumber,...fivenumber]
+console.log(allnumber)
+
+const additionalinfo ={hobby:'foot',education:'telecom Eng'}
+const allinfo={...generalinfo,...additionalinfo};
+console.log(allinfo)
+
+
+
+
