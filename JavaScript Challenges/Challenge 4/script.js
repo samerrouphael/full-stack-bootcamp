15953b311e08ed58c071731285e9503ea6c16abb
samer <rouphaels@gmail.com>
2023-08-22 15:35:44 +0300

    challemge

---
+++ JavaScript Challenges/Challenge 4/script.js
@@ -0,0 +1,6 @@
+let fivenumber =[1,2,3,4,5] 
+console.log(fivenumber)
+
+const generalinfo ={ Name :'Samer', age :'36',height:'180',location:'newrawda',nationality :'lebanese'}
+console.log(generalinfo)
+
