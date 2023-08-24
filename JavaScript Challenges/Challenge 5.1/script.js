2bab8ca4a83e0f5c47c104dc61756133ed591c8f
samer <rouphaels@gmail.com>
2023-08-24 15:34:06 +0300

    challenge5.1 step2 done

--- JavaScript Challenges/Challenge 5.1/script.js
+++ JavaScript Challenges/Challenge 5.1/script.js
@@ -2,43 +2,56 @@
 let button = document.createElement("button");
 document.body.appendChild(button);
 
-button.textContent='button1'
-button.style.padding ='10px 20px'
-button.style.fontsize='16px'
-button.style.backgroundcolor='#3498db'
-button.style.color='#ffffff'
-button.style.border ='none'
-button.style.cursor='pointer'
+button.textContent = 'button1'
+button.style.padding = '10px 20px'
+button.style.fontsize = '16px'
+button.style.backgroundcolor = '#3498db'
+button.style.color = '#ffffff'
+button.style.border = 'none'
+button.style.cursor = 'pointer'
 
-button.addEventListener("click",function(){
+button.addEventListener("click", function () {
     button.style.backgroundColor = "red"
-    button.textContent='10'
+    button.textContent = '10'
 })
 
 let para = document.createElement('h1');
 document.body.appendChild(para);
 para.textContent = "hello world";
-para.style.width="10px";
-para.style.color='green';
-para.style="dotted";
+para.style.border = '10px dotted green'
 
-let input = document.querySelector('input')
 
-input.addEventListener('keydown',(a)=>{ 
-    h1.style.backgroundColor='red'
-    
-    input.addEventListener('keydown',(s)=>{ 
-        h1.style.padding='10px'    
-    })
-    input.addEventListener('keydown',(d)=>{ 
+
+document.addEventListener('keydown', function (event) {
+    if (event.key === 'a' || event.key === 'A') {
+        para.style.backgroundColor = 'red'
+    }
+})
+
+document.addEventListener('keydown', function (event) {
+    if (event.key === 's' || event.key === 'S') {
+        para.style.padding = 'red'
+    }
+})
+document.addEventListener('keydown', function (event) {
+    if (event.key === 'w' || event.key === 'W') {
+        para.toggleAttribute = 'visible'
+    }
+})
+ocument.addEventListener('keydown', function (event) {
+    if (event.key === 'd' || event.key === 'D') {
         let para2 = document.createElement('p');
         document.body.appendChild(para2);
-    })
-    input.addEventListener('keydown',(w)=>{ 
-        h1.toggle='visible'
-    })
-    input.addEventListener('keydown',(space)=>{ 
-        h1.style.fontsize='100px'
-    })
+        para.textContent = "samer";
+    }
 })
+
+document.addEventListener('keydown', function (event) {
+    if (event.key === 'space') {
+        para.style.fontsize = '50px'
+    }
+})
+
+
+
 
